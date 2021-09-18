3310eca0a40d0ffe697a7248970a16bf2cf30551
Jini2 <10_cal@naver.com>
2021-09-19 01:37:52 +0900

    change design

--- gitbook/gitbook-custom.js
+++ gitbook/gitbook-custom.js
@@ -1,12 +1,23 @@
-require([
-    'jquery'
-], function($) {
-    $(function(){
-        $('.cat-link').on('click',  e => {
-            toggleCategory($(e.currentTarget));
-        });
-    });
+require(['gitbook', 'jquery'], function(gitbook, $) {
+    //sidebar default hide
+    gitbook.storage.set('sidebar', false);
+    
+    function resize(){
+        if($(window).width() < 570) {
+            $(".header-search-wrapper").hide();
+            $(".nav-app-wrapper").hide();
+        }else{
+            $(".header-search-wrapper").show();
+            $(".nav-app-wrapper").show();
+        }
+    }
 
+    resize();
+    $(window).resize(e => {
+        resize();
+    });
+    
+    //category expand toggle
     function toggleCategory(el) {
         if(el.hasClass('cat-show')){
             el.next().hide();
@@ -18,4 +29,9 @@ require([
             el.find('.cat-icon').removeClass('fa-caret-right').addClass('fa-caret-down');
         }
     }
+
+    $(document).on('click', '.cat-link',  e => {
+        toggleCategory($('.cat-link.cat-show'));
+        toggleCategory($(e.currentTarget));
+    });
 });
--- index.md
+++ index.md
@@ -2,4 +2,12 @@
 layout: home
 ---
 
-Today Jini2 Learned
+# <span style="color:mediumseagreen">안녕하세요.<br>저는 강현지입니다.</span>    
+
+#### JAVA/SPRING 기반 7년차, 풀스택 개발자로 일하고 있습니다.<br>
+
+
+
+##### Contact.
+> E-MAIL : <10_cal@naver.com>  
+> GitHub : [https://github.com/Jini2](https://github.com/Jini2)
